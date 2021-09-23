569046ea8706b9b1ffb90e39571594a814d10575
victorChekhovoy <viktor.chekhovoi@gmail.com>
2021-09-22 19:06:12 -0500

    some progress

--- assignments/assignment3/main.scm
+++ assignments/assignment3/main.scm
@@ -8,6 +8,7 @@
   (lambda (bst)
     (cond ((not (list? bst)) #f)
           ((not (equal? (length bst) 3)) #f)
+          ((not (number? (car bst))) #f)
           ((not (list? (car (cdr bst)))) #f)
           ((not (list? (car (cdr (cdr bst))))) #f)
           (else #t))))
@@ -30,10 +31,26 @@
       #f
       (car (cdr (cdr bst))))))
 
+(define make-bst
+  (lambda (elt left-bst right-bst)
+    (cond ((not (number? elt)) #f)
+          ((not (list? left-bst)) #f)
+          ((not (list? right-bst)) #f))
+
+    (if (eq? (length left-bst) 3)
+      (cond ((not (list? (car (cdr left-bst)))) #f)
+            ((not (list? (car (cdr (cdr left-bst))))) #f))
+      (if (not (null? left-bst))
+        #f))
+    (if (eq? (length right-bst) 3)
+       (cond ((not (list? (car (cdr right-bst)))) #f)
+             ((not (list? (car (cdr (cdr right-bst))))) #f))
+       (if (not (null? right-bst))
+         #f))
 
 
-(define bst '(5 (3 () (4 () ()) ) ()))
 
-(entry bst)
-(left bst)
-(right bst)
+    ;cons elt (cons left-bst (cons right-bst '())))))
+
+
+(make-bst 5 '() '())
